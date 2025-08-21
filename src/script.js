9781dbfeef84f5b8dc3bee908a466c67cce961a8
alex <alexpham66@gmail.com>
2025-08-20 18:43:59 -0600

    code
    code

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const animatedElements = document.querySelectorAll('[data-animate]');
+
+
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if (entry.isIntersecting) {
+entry.target.classList.add('visible');
+}
+});
+}, { threshold: 0.2 });
+
+
+animatedElements.forEach(el => observer.observe(el));
